404367c3b276f95c5fd97bab34cefa6f13ea0a3e
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-07-12 22:11:50 +0900

    Add fan object.

---
+++ Assets/MyAssets/Prefabs/RubeGoldbergs/FanHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerStay(Collider col)
+	{
+		Rigidbody colRigidbody = col.GetComponent<Rigidbody>();
+        colRigidbody.AddForce(30.0f* transform.forward, ForceMode.Acceleration);
+	}
+}
