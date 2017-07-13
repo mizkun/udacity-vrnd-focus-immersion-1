5720d5cde329a26fe1a38841989cc1a2b7388341
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-07-13 23:03:11 +0900

    Add basic game logic.

---
+++ Assets/MyAssets/Scripts/BallHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHandler : MonoBehaviour
+{
+    public StarHandler starHandler;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            ReStart();
+        }
+        else if (col.gameObject.CompareTag("Star"))
+        {
+            col.gameObject.SetActive(false);
+            starHandler.AddScore();
+        }
+        else if (col.gameObject.CompareTag("Goal"))
+        {
+            if (starHandler.IsCompleted())
+            {
+                SteamVR_LoadLevel.Begin("ExampleScene");
+            }
+            else 
+            {
+                ReStart();
+            }
+        }
+    }
+
+    void ReStart () {
+		transform.position = new Vector3(0, 1.15f, -2.6f);
+		Rigidbody rigidBody = transform.GetComponent<Rigidbody>();
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.angularVelocity = Vector3.zero;
+		rigidBody.isKinematic = false;
+		starHandler.ReStart();
+    }
+}
---
+++ Assets/MyAssets/Scripts/StarHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarHandler : MonoBehaviour {
+    public GameObject[] Stars;
+    int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReStart ()
+    {
+        foreach (GameObject star in Stars)
+        {
+            star.SetActive(true);
+        }
+        score = 0;
+    }
+
+    public void AddScore () 
+    {
+        score++;
+        Debug.Log(score);
+    }
+
+    public bool IsCompleted () 
+    {
+        return score == Stars.Length;
+    }
+
+
+}
